0aa83ccd798fd0a6091586e1e14dee8c35c4759f
Jemma <jemmamillman@gmail.com>
2019-04-09 09:30:10 +0100

    exercise 30 added

--- src/main/java/exercise30/MultiplicationTable.java
+++ src/main/java/exercise30/MultiplicationTable.java
@@ -1,4 +1,16 @@
 package exercise30;
 
 public class MultiplicationTable {
+
+    String multiplication;
+
+    public String timesTables() {
+
+        for (int i = 0; i < 13; i++) {
+            for (int j = 0; j < 13; j++) {
+                multiplication = i + " * " + j + " = " + i * j;
+            }
+        }
+        return multiplication;
+    }
 }
