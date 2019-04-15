0eca51c9e7bace2a85273a617ca420257b1c1046
alec.tunbridge <alec.tunbridge@autotrader.co.uk>
2019-04-15 15:07:18 +0100

    Few comments added to exercise 31.

--- src/main/java/exercise31/KarvonenHeartRate.java
+++ src/main/java/exercise31/KarvonenHeartRate.java
@@ -2,6 +2,7 @@ package exercise31;
 
 public class KarvonenHeartRate {
 
+    //Data type?
     private static String[] table = new String[9];
 
     private int index = 0;
@@ -13,19 +14,24 @@ public class KarvonenHeartRate {
         return bpm;
     }
 
+    //Naming / data-type?
     public String[] checkDifferentIntensities(int age, int restingHR) {
+        //Shorter way to do addition?
         for (double intensity = 55; intensity <= 95; intensity = intensity + 5) {
+            //Brackets and whitespace?
             table[index] = (formula(age, restingHR, intensity) + "");
 
+            //Why the additional counter?
             if(index < table.length){
                 index++;
             }
 
         }
-
+        //What about printing out the whole table?
         return table;
     }
 
+    //What's this main method doing?
     public static void main(String[] args) {
         KarvonenHeartRate karvonenHeartRate = new KarvonenHeartRate();
         karvonenHeartRate.checkDifferentIntensities(25, 65);
