e63cefb0448d459ff3280f984a0211651d923e53
Jemma <jemmamillman@gmail.com>
2019-04-18 10:45:44 +0100

    refactored 38

--- src/main/java/exercise39/SortingRecords.java
+++ src/main/java/exercise39/SortingRecords.java
@@ -1,4 +1,27 @@
 package exercise39;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class SortingRecords {
+
+    public static void main(String[] args) {
+
+        List <String>lastName = new ArrayList<>();
+
+        lastName.add("Johnson");
+        lastName.add("Xiong");
+        lastName.add("Michaelson");
+        lastName.add("Jacobson");
+        lastName.add("Jackson");
+        lastName.add("Weber");
+
+        Map<String, List<String>> sortingRecords = new HashMap<>();
+
+        sortingRecords.put("Last Name", lastName);
+
+        System.out.println(sortingRecords);
+    }
 }
