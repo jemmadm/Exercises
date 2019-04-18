7219c93c456592bae085341c763b7e169a377f0d
Jemma <jemmamillman@gmail.com>
2019-04-18 11:31:17 +0100

    started 39

--- src/main/java/exercise39/SortingRecords.java
+++ src/main/java/exercise39/SortingRecords.java
@@ -1,27 +1,22 @@
 package exercise39;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class SortingRecords {
 
-    public static void main(String[] args) {
+    public List<String> recordSorter() {
 
-        List <String>lastName = new ArrayList<>();
+        Map<String, String> sortingRecords = new HashMap<>();
 
-        lastName.add("Johnson");
-        lastName.add("Xiong");
-        lastName.add("Michaelson");
-        lastName.add("Jacobson");
-        lastName.add("Jackson");
-        lastName.add("Weber");
+        sortingRecords.put("Johnson","John Manager 2016-2-31");
+        sortingRecords.put("Xiong","Tou Software Engineer 2016-10-05");
+        sortingRecords.put("Michaelson","Michaela District Manager 20165-12-19");
+        sortingRecords.put("Jacobson","Jake Programmer 2016-10-05");
+        sortingRecords.put("Jackson","Jacquelyn DBA");
+        sortingRecords.put("Weber","Sally Web Developer 2015-12-18");
 
-        Map<String, List<String>> sortingRecords = new HashMap<>();
-
-        sortingRecords.put("Last Name", lastName);
-
-        System.out.println(sortingRecords);
+        List<String> lastNameByKey = new ArrayList<>(sortingRecords.values());
+        Collections.sort(lastNameByKey);
+        return lastNameByKey;
     }
 }
--- src/test/java/exercise39/SortingRecordsTest.java
+++ src/test/java/exercise39/SortingRecordsTest.java
@@ -1,7 +1,19 @@
 package exercise39;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class SortingRecordsTest {
+
+    SortingRecords sortingRecords = new SortingRecords();
+
+    @Test
+
+    public void doesSortWork(){
+
+        assertThat(sortingRecords.recordSorter()).isEqualTo("Jacquelyn DBA\n Jake Programmer 2016-10-05\n John Manager 2016-2-31\n Michaela District Manager 20165-12-19\n Sally Web Developer 2015-12-18\n Tou Software Engineer 2016-10-05");
+
+    }
 
 }
