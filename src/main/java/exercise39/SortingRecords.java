316bb585ec45fc93cced9ebb84bf1136d5134a3c
Jemma <jemmamillman@gmail.com>
2019-04-18 11:32:44 +0100

    started 39

--- src/main/java/exercise39/SortingRecords.java
+++ src/main/java/exercise39/SortingRecords.java
@@ -8,12 +8,12 @@ public class SortingRecords {
 
         Map<String, String> sortingRecords = new HashMap<>();
 
-        sortingRecords.put("Johnson","John Manager 2016-2-31");
-        sortingRecords.put("Xiong","Tou Software Engineer 2016-10-05");
-        sortingRecords.put("Michaelson","Michaela District Manager 20165-12-19");
-        sortingRecords.put("Jacobson","Jake Programmer 2016-10-05");
-        sortingRecords.put("Jackson","Jacquelyn DBA");
-        sortingRecords.put("Weber","Sally Web Developer 2015-12-18");
+        sortingRecords.put("Johnson", "John Manager 2016-2-31");
+        sortingRecords.put("Xiong", "Tou Software Engineer 2016-10-05");
+        sortingRecords.put("Michaelson", "Michaela District Manager 20165-12-19");
+        sortingRecords.put("Jacobson", "Jake Programmer 2016-10-05");
+        sortingRecords.put("Jackson", "Jacquelyn DBA");
+        sortingRecords.put("Weber", "Sally Web Developer 2015-12-18");
 
         List<String> lastNameByKey = new ArrayList<>(sortingRecords.values());
         Collections.sort(lastNameByKey);
--- src/test/java/exercise39/SortingRecordsTest.java
+++ src/test/java/exercise39/SortingRecordsTest.java
@@ -9,11 +9,8 @@ public class SortingRecordsTest {
     SortingRecords sortingRecords = new SortingRecords();
 
     @Test
-
-    public void doesSortWork(){
-
+    public void doesSortWork() {
         assertThat(sortingRecords.recordSorter()).isEqualTo("Jacquelyn DBA\n Jake Programmer 2016-10-05\n John Manager 2016-2-31\n Michaela District Manager 20165-12-19\n Sally Web Developer 2015-12-18\n Tou Software Engineer 2016-10-05");
 
     }
-
 }
