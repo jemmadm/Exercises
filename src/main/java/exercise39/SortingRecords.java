badbf5fadc946957e15adb141e9fda01a2642491
Jemma <jemmamillman@gmail.com>
2019-04-23 11:54:14 +0100

    refactored SortingRecords class

--- src/main/java/exercise39/SortingRecords.java
+++ src/main/java/exercise39/SortingRecords.java
@@ -4,51 +4,29 @@ import java.util.*;
 
 public class SortingRecords {
 
+    public Map<String, String> createRecord(String firstName, String lastName, String position, String date){
+        Map<String, String> map = new HashMap<>();
+        map.put("First Name", firstName);
+        map.put("Last Name", lastName);
+        map.put("Position", position);
+        map.put("Date", date);
+
+        return map;
+    }
+
     public String recordSorter() {
 
-        String lastNames = "";
-        String firstNames = "";
-        String position = "";
-        String date = "";
         String unsortedRecords = "";
 
-        Map<String, String> johnMap = new HashMap<>();
-        johnMap.put("First Name", "John");
-        johnMap.put("Last Name", "Johnson");
-        johnMap.put("Position", "Manager");
-        johnMap.put("Date", "2016-12-31");
-
-        Map<String, String> touMap = new HashMap<>();
-        touMap.put("First Name", "Tou");
-        touMap.put("Last Name", "Xiong");
-        touMap.put("Position", "Software Engineer");
-        touMap.put("Date", "2016-10-05");
-
-        Map<String, String> michaelaMap = new HashMap<>();
-        michaelaMap.put("First Name", "Michaela");
-        michaelaMap.put("Last Name", "Michaelson");
-        michaelaMap.put("Position", "District Manager");
-        michaelaMap.put("Date", "2015-12-19");
-
-        Map<String, String> jakeMap = new HashMap<>();
-        jakeMap.put("First Name", "Jake");
-        jakeMap.put("Last Name", "Jacobson");
-        jakeMap.put("Position", "Programmer");
-        jakeMap.put("Date", "");
-
-        Map<String, String> jacquelynMap = new HashMap<>();
-        jacquelynMap.put("First Name", "Jacquelyn");
-        jacquelynMap.put("Last Name", "Jackson");
-        jacquelynMap.put("Position", "DBA");
-        jacquelynMap.put("Date", "");
-
-        Map<String, String> sallyMap = new HashMap<>();
-        sallyMap.put("First Name", "Sally");
-        sallyMap.put("Last Name", "Weber");
-        sallyMap.put("Position", "Web Developer");
-        sallyMap.put("Date", "2015-12-18");
+        Map<String, String> johnMap = createRecord("John", "Johnson", "Manager", "2016-12-31");
+        Map<String, String> touMap = createRecord("Tou", "Xiong", "Software Engineer", "2016-10-05");
+        Map<String, String> michaelaMap = createRecord("Michaela", "Michaelson", "District Manager", "2015-12-19");
+        Map<String, String> jakeMap = createRecord("Jake", "Jacobson", "Programmer", "");
+        Map<String, String> jacquelynMap = createRecord("Jacquelyn", "Jackson", "DBA", "");
+        Map<String, String> sallyMap = createRecord("Sally", "Weber", "Web Developer", "2015-12-18");
 
         List<Map<String, String>> records = new ArrayList<>();
+
         records.add(johnMap);
         records.add(touMap);
         records.add(michaelaMap);
@@ -57,7 +35,7 @@ public class SortingRecords {
         records.add(sallyMap);
 
         for (Map<String, String> map : records) {
-            unsortedRecords += map.get("First Name") + " " + map.get("Last Name") + " " + position + map.get("Position") + " " + date + map.get("Date") + "\n";
+            unsortedRecords += map.get("First Name") + " " + map.get("Last Name") + " " + map.get("Position") + " " + map.get("Date") + "\n";
         }
         return unsortedRecords.trim();
     }
