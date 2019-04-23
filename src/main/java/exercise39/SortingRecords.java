b87c1c4ca127ab2131eadbaf61bf3bf7da9eaddb
Jemma <jemmamillman@gmail.com>
2019-04-23 14:15:55 +0100

    overloaded createRecords method

--- src/main/java/exercise39/SortingRecords.java
+++ src/main/java/exercise39/SortingRecords.java
@@ -5,12 +5,15 @@ import java.util.*;
 public class SortingRecords {
 
     public Map<String, String> createRecord(String firstName, String lastName, String position, String date){
+        Map<String, String> map = createRecord(firstName, lastName, position);
+        map.put("Date", date);
+        return map;
+    }
+    public Map<String, String> createRecord(String firstName, String lastName, String position){
         Map<String, String> map = new HashMap<>();
         map.put("First Name", firstName);
         map.put("Last Name", lastName);
         map.put("Position", position);
-        map.put("Date", date);
-
         return map;
     }
 
@@ -21,8 +24,8 @@ public class SortingRecords {
         Map<String, String> johnMap = createRecord("John", "Johnson", "Manager", "2016-12-31");
         Map<String, String> touMap = createRecord("Tou", "Xiong", "Software Engineer", "2016-10-05");
         Map<String, String> michaelaMap = createRecord("Michaela", "Michaelson", "District Manager", "2015-12-19");
-        Map<String, String> jakeMap = createRecord("Jake", "Jacobson", "Programmer", "");
-        Map<String, String> jacquelynMap = createRecord("Jacquelyn", "Jackson", "DBA", "");
+        Map<String, String> jakeMap = createRecord("Jake", "Jacobson", "Programmer");
+        Map<String, String> jacquelynMap = createRecord("Jacquelyn", "Jackson", "DBA");
         Map<String, String> sallyMap = createRecord("Sally", "Weber", "Web Developer", "2015-12-18");
 
         List<Map<String, String>> records = new ArrayList<>();
@@ -35,10 +38,11 @@ public class SortingRecords {
         records.add(sallyMap);
 
         for (Map<String, String> map : records) {
+            String date = map.get("Date");
             unsortedRecords += map.get("First Name") + " " + map.get("Last Name") + " " + map.get("Position");
 
-            if (!map.get("Date").isEmpty()) {
-                unsortedRecords += " " + map.get("Date");
+            if (date!=null&&!date.isEmpty()) {
+                unsortedRecords += " " + date;
             }
             unsortedRecords += "\n";
         }
